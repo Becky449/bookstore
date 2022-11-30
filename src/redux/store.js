83a40beb020d1e37695aef160ce34e0639f78fc3
Becky449 <rabnab12@gmail.com>
2022-11-30 21:24:30 +0300

    imported the books reducer and added it using combineReducers.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import categoriesReducer from './categories/categories';
+import booksReducer from './books/books';
 
 const reducer = combineReducers({
   categories: categoriesReducer,
+  books: booksReducer,
 });
 
 const store = configureStore({
