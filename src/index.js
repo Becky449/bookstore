a460560b1b6ebb12737ca4cf3e4dd0a77191b2bd
Becky449 <rabnab12@gmail.com>
2022-12-03 16:32:10 +0300

    Fix errors

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import BookStore from './components/BookStore';
+import Books from './components/BookStore';
 import store from './redux/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BookStore />
+      <Books />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
