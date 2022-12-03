a805fdacc8d1254e40f98cc8803cd48efdf4feec
Becky449 <rabnab12@gmail.com>
2022-12-03 12:56:16 +0300

    Fix bug error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import BookStore from 'components/BookStore';
+import BookStore from './components/BookStore';
 import store from './redux/configureStore';
 
 ReactDOM.render(
