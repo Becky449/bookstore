cedff730152834f4f4ee0b19ab30686663ffd42f
Becky449 <rabnab12@gmail.com>
2022-12-05 10:44:52 +0300

    Fix errors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Navigation from './Components/Navigation';
-import Booklist from './Components/booklist';
-import Categories from './Components/Categories';
+import Navigation from './components/Navigation';
+import Booklist from './components/Booklist';
+import Categories from './components/Categories';
 import store from './redux/configureStore';
 
 function App() {
